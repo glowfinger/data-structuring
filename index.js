9b9fa3c462c74ea7ce838b6b2ffc30d68bca9d95
Darren Lilley <darren@glowfinger.com>
2024-01-03 19:51:48 +0000

    Clean up

--- index.js
+++ index.js
@@ -1,53 +1,60 @@
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 import { faker } from '@faker-js/faker';
-
-
+import cliProgress from 'cli-progress';
+import colors from 'ansi-colors';
 // Create a connection to the database
-const connection = mysql.createConnection({
+const connection = await mysql.createConnection({
     host: '127.0.0.1',
-    port: 3307,
-    user: 'fast',
-    password: 'fast',
-    database: 'fast'
+    port: 3306,
+    user: 'db',
+    password: 'db',
+    database: 'db'
 });
 
-
-
-
-
-let max = 100000;
-
-
-for (let c = 0; c < max; c++) {
-
-    const users = [];
-    for (let i = 0; i < 10; i++) {
-
-        const firstName = faker.person.firstName();
-        const lastName = faker.person.lastName();
-        const email = `${firstName}.${lastName}@${faker.internet.domainName()}`.toLowerCase();
-
-        const user = {
-            email,
-            firstName,
-            lastName,
-            dob: faker.date.past(),
-            status: faker.helpers.arrayElement(['active', 'inactive', 'pending', 'deleted', 'suspended', 'banned', 'archived', 'locked', 'flagged', 'verified', 'unverified',]),
-            region: faker.location.country().toLocaleLowerCase(),
-            type: faker.helpers.arrayElement(['admin', 'user', 'moderator', 'manager', 'owner', 'developer', 'tester', 'support', 'customer', 'guest',])
-        };
-        users.push(user);
-    }
-
-    const query = 'INSERT INTO user (email, first_name, last_name, dob, status, region, type) VALUES ?';
-connection.query(query, [users.map(user => [user.email, user.firstName, user.lastName, user.dob, user.status, user.region, user.type])], (error, results) => {
-    if (error) {
-        console.error('Error inserting users:', error);
-    }
+const b1 = new cliProgress.SingleBar({
+    format: 'Progress |' + colors.cyan('{bar}') + '| {percentage}%',
+    barCompleteChar: '\u2588',
+    barIncompleteChar: '\u2591',
+    hideCursor: true
 });
 
+let max = 3000000;
+let i = 0;
+
+b1.start(max, 0, {
+    speed: "N/A"
+});
+
+while (i < max) {
+
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+    const email = `${firstName}.${lastName}@${faker.internet.domainName()}`.toLowerCase();
+
+    const user = {
+        email,
+        firstName,
+        lastName,
+        dob: faker.date.past(),
+        status: faker.helpers.arrayElement(['active', 'inactive', 'pending', 'deleted', 'suspended', 'banned', 'archived', 'locked', 'flagged', 'verified', 'unverified',]),
+        region: faker.location.country().toLocaleLowerCase(),
+        type: faker.helpers.arrayElement(['admin', 'user', 'moderator', 'manager', 'owner', 'developer', 'tester', 'support', 'customer', 'guest',])
+    };
+
+    const query = 'INSERT INTO data (email, first_name, last_name, dob, status, region) VALUES (?, ?, ?, ?, ?, ?)';
+
+
+    try {
+        await connection.query(query, [user.email, user.firstName, user.lastName, user.dob, user.status, user.region]);
+    } catch (error) {
+        console.error( error.sql);
+    }
+
+    i++;
+    b1.update(i);
 }
 
-connection.end();
+b1.stop();
 
+connection.end();
 
---
+++ sql/db.sql
@@ -0,0 +1,42 @@
+CREATE TABLE data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    dob DATE NOT NULL,
+    status VARCHAR(100) NOT NULL,
+    region VARCHAR(100) NOT NULL,
+    phone VARCHAR(100),
+    instagram VARCHAR(100),
+    twitter VARCHAR(100),
+    facebook VARCHAR(100),
+    snapchat VARCHAR(100)
+);
+
+
+
+
+CREATE TABLE user_bio (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);
+CREATE INDEX idx_lastname
+ON Persons (LastName);
+
+CREATE TABLE user_dob (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    dob DATE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+CREATE TABLE user_contact (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    method VARCHAR(100) NOT NULL INDEX,
+    info VARCHAR(100) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);
